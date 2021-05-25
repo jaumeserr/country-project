1f824b68960179d97a1ce41ca6664ae372d3fe1d
Jaume Serradell <jaumeserr@gmail.com>
2021-05-25 13:45:28 +0200

    Create project

---
+++ src/components/CountryList.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import getCountries from "../services/getCountries";
+import Country from "../components/Country"
+
+const CountryList = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries().then((country) => setCountries(country));
+  }, []);
+
+  return (
+    <div>
+      {countries.map((country) => (
+        <Country key={country.alpha3Code} {...country} />
+      ))}
+    </div>
+  );
+};
+
+export default CountryList;
